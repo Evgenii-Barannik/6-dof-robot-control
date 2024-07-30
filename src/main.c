72c864ba7ef16b0f0630f2aebfa0e670039f5999
Evgenii-Barannik <radchemsu@gmail.com>
2024-07-30 17:42:27 +0300

    Needle end is now the center of coordinates

--- src/main.c
+++ src/main.c
@@ -19,20 +19,20 @@ const int NUM_OF_SLIDERS = 6;
 
 
 /// CONSTANTS THAT CAN BE CHANGED
-const  double NEEDLE_LENGTH = 3;
+const double NEEDLE_LENGTH = 3;
 const double TABLE_RADIUS = 3.0; // Radius of the hexagonal table in cm.
-const double TABLE_ANGLE = DEGREES_TO_RADIANS * 80; // Angle between vertex pairs of the hexagonal table in radians. A regular hexagon will have 60 deg.
+const double TABLE_ANGLE = DEGREES_TO_RADIANS * 80; // Angle between vertices of the hexagonal table in radians. A regular hexagon will have 60 deg.
 const double BASE_RADIUS = 7.0; // Radius of the hexagonal base in cm.
-const double BASE_ANGLE = DEGREES_TO_RADIANS * 15; // Angle between vertex pairs of the hexagonal base in radians. A regular hexagon will have 60 deg.
+const double BASE_ANGLE = DEGREES_TO_RADIANS * 15; // Angle between vertices of the hexagonal base in radians. A regular hexagon will have 60 deg.
 const double MAX_POSSIBLE_HEIGHT = 15.0; // Highest possible position for sliders in cm.
 const double MIN_POSSIBLE_HEIGHT = 0; // Lowest possible position for sliders in cm.
 const double ARM_LENGTH = 8.0; // Length of all robot arms in cm.
 ///
 
 
-struct TableCoordinates { // Full description of the table position.
-    double x, y, z; // Coordinates of the table center in cm.
-    double phi, theta, psi; // Euler angles for the table in radians.
+struct NeedleCoordinates { // Full ideal description of the needle position, and thus, whole system.
+    double x, y, z; // Coordinates of the needle end in cm.
+    double phi, theta, psi; // Euler angles for the needle in radians.
 };
 
 // Array of pointers to NULL (if slider coordinates were not found)
@@ -51,14 +51,14 @@ bool all_not_NULL(const OptionSliderCoordinates* options) {
     return true;
 }
 
-void print_targeted_table_coordinates(struct TableCoordinates table) {
+void print_targeted_needle_coordinates(struct NeedleCoordinates table) {
     printf("\n");
-    printf("Targeted table coordinates: \n");
-    printf("Centroid (x, y, z) coordinates in cm:      (%f, %f, %f)\n", table.x, table.y, table.z);
+    printf("Targeted needle coordinates: \n");
+    printf("Linear coordinates (x, y, z) in cm:        (%f, %f, %f)\n", table.x, table.y, table.z);
     printf("Euler angles (phi, theta, psi) in radians: (%f, %f, %f)\n", table.phi, table.theta, table.psi);
 }
 
-void print_table_vertices_and_centroid_and_needle(gsl_vector** vectors, int num_of_vectors_to_print) {
+void print_3dmodel_coordinates(gsl_vector** vectors, int num_of_vectors_to_print) {
     assert(num_of_vectors_to_print == NUM_OF_HEXAGON_VERTICES + 2);
 
     for(int i = 0; i < num_of_vectors_to_print; i++) {
@@ -119,8 +119,7 @@ void print_transformed_slider_coordinates(const OptionSliderCoordinates option) 
 
 void populate_transformation_matrix(
     gsl_matrix* transformation_matrix,
-    const struct TableCoordinates c) {
-
+    const struct NeedleCoordinates c) {
     gsl_matrix_set(transformation_matrix, 0, 0, cos(c.psi) * cos(c.theta));
     gsl_matrix_set(transformation_matrix, 0, 1, cos(c.psi) * sin(c.theta) * sin(c.phi) - sin(c.psi) * cos(c.phi));
     gsl_matrix_set(transformation_matrix, 0, 2, cos(c.psi) * sin(c.theta) * cos(c.phi) + sin(c.psi) * sin(c.phi));
@@ -142,13 +141,52 @@ void populate_transformation_matrix(
     gsl_matrix_set(transformation_matrix, 3, 3, 1.0);
 }
 
+void populate_table_3dmodel (gsl_vector** coordinates) {
+    double radius = TABLE_RADIUS;
+    double angle_in_radians = TABLE_ANGLE;
+    double separation_from_needle_end = NEEDLE_LENGTH;
+
+    gsl_vector_set(coordinates[0], 0, radius * sin(DEGREES_TO_RADIANS * 0 + (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[0], 1, radius * cos(DEGREES_TO_RADIANS * 0 + (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[0], 2, separation_from_needle_end);
+
+    gsl_vector_set(coordinates[1], 0, radius * sin(DEGREES_TO_RADIANS * 0 - (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[1], 1, radius * cos(DEGREES_TO_RADIANS * 0 - (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[1], 2, separation_from_needle_end);
+
+    gsl_vector_set(coordinates[2], 0, radius * sin(DEGREES_TO_RADIANS * 120 + (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[2], 1, radius * cos(DEGREES_TO_RADIANS * 120 + (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[2], 2, separation_from_needle_end);
+
+    gsl_vector_set(coordinates[3], 0, radius * sin(DEGREES_TO_RADIANS * 120 - (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[3], 1, radius * cos(DEGREES_TO_RADIANS * 120 - (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[3], 2, separation_from_needle_end);
+
+    gsl_vector_set(coordinates[4], 0, radius * sin(DEGREES_TO_RADIANS * 240 + (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[4], 1, radius * cos(DEGREES_TO_RADIANS * 240 + (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[4], 2, separation_from_needle_end);
+
+    gsl_vector_set(coordinates[5], 0, radius * sin(DEGREES_TO_RADIANS * 240 - (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[5], 1, radius * cos(DEGREES_TO_RADIANS * 240 - (angle_in_radians / 2.0)));
+    gsl_vector_set(coordinates[5], 2, separation_from_needle_end);
+
+    // Table centroid
+    gsl_vector_set(coordinates[6], 0, 0.0);
+    gsl_vector_set(coordinates[6], 1, 0.0);
+    gsl_vector_set(coordinates[6], 2, separation_from_needle_end);
+
+    // Needle end
+    gsl_vector_set(coordinates[7], 0, 0.0);
+    gsl_vector_set(coordinates[7], 1, 0.0);
+    gsl_vector_set(coordinates[7], 2, 0.0);
+}
+
 // Used to populate both table and base coordinates.
 // Base coordinates are coordinates of sliders at their lowest possible position at rest.
 void populate_hexagon(
     gsl_vector** coordinates,
     const double radius,
     const double angle_in_radians) {
-
     gsl_vector_set(coordinates[0], 0, radius * sin(DEGREES_TO_RADIANS * 0 + (angle_in_radians / 2.0)));
     gsl_vector_set(coordinates[0], 1, radius * cos(DEGREES_TO_RADIANS * 0 + (angle_in_radians / 2.0)));
     gsl_vector_set(coordinates[0], 2, 0.0);
@@ -172,36 +210,6 @@ void populate_hexagon(
     gsl_vector_set(coordinates[5], 0, radius * sin(DEGREES_TO_RADIANS * 240 - (angle_in_radians / 2.0)));
     gsl_vector_set(coordinates[5], 1, radius * cos(DEGREES_TO_RADIANS * 240 - (angle_in_radians / 2.0)));
     gsl_vector_set(coordinates[5], 2, 0.0);
-}
-
-void populate_centroid_coordinates(gsl_vector** coordinates) {
-    double x_sum = 0.0, y_sum = 0.0, z_sum = 0.0;
-    for (int j = 0; j < NUM_OF_HEXAGON_VERTICES; j++) {
-        x_sum += gsl_vector_get(coordinates[j], 0);
-        y_sum += gsl_vector_get(coordinates[j], 1);
-        z_sum += gsl_vector_get(coordinates[j], 2);
-    }
-    double x_centroid = x_sum/NUM_OF_HEXAGON_VERTICES;
-    double y_centroid = y_sum/NUM_OF_HEXAGON_VERTICES;
-    double z_centroid = z_sum/NUM_OF_HEXAGON_VERTICES;
-
-    gsl_vector_set(coordinates[NUM_OF_HEXAGON_VERTICES], 0, x_centroid);
-    gsl_vector_set(coordinates[NUM_OF_HEXAGON_VERTICES], 1, y_centroid);
-    gsl_vector_set(coordinates[NUM_OF_HEXAGON_VERTICES], 2, z_centroid);
-}
-
-void populate_needle_coordinates(gsl_vector** coordinates_with_populated_centroid) {
-    double coordinate_x_centroid = gsl_vector_get(coordinates_with_populated_centroid[NUM_OF_HEXAGON_VERTICES], 0);
-    double coordinate_y_centroid = gsl_vector_get(coordinates_with_populated_centroid[NUM_OF_HEXAGON_VERTICES], 1);
-    double coordinate_z_centroid = gsl_vector_get(coordinates_with_populated_centroid[NUM_OF_HEXAGON_VERTICES], 2);
-
-    double coordinate_x_needle = coordinate_x_centroid;
-    double coordinate_y_needle = coordinate_y_centroid;
-    double coordinate_z_needle = coordinate_z_centroid - NEEDLE_LENGTH;
-
-    gsl_vector_set(coordinates_with_populated_centroid[NUM_OF_HEXAGON_VERTICES+1], 0, coordinate_x_needle);
-    gsl_vector_set(coordinates_with_populated_centroid[NUM_OF_HEXAGON_VERTICES+1], 1,  coordinate_y_needle);
-    gsl_vector_set(coordinates_with_populated_centroid[NUM_OF_HEXAGON_VERTICES+1], 2, coordinate_z_needle);
 }
 
 void populate_transformed_coordinates(
@@ -228,8 +236,6 @@ void populate_transformed_coordinates(
     gsl_vector_free(initial_coordinates_in_4D);
     gsl_vector_free(transformed_coordinates_in_4D);
 }
-
-
 
 OptionSliderCoordinates find_slider_coordinates(
     gsl_vector** table_transformed_coordinates,
@@ -285,33 +291,33 @@ OptionSliderCoordinates find_slider_coordinates(
 // This movement can damage our mechanical system.
 
 int main(void) {
-    struct TableCoordinates targeted_table_coordinates = {0.5, -0.5, 5.0, -M_PI/10, M_PI/3, 0};
+    struct NeedleCoordinates targeted_needle_coordinates = {0.5, 0.5, 3.0, -M_PI/10, M_PI/10, M_PI/10};
 
     gsl_matrix* transformation_matrix = gsl_matrix_alloc(4, 4);
-    populate_transformation_matrix(transformation_matrix, targeted_table_coordinates);
+    // Transformation that brings point (0, 0, 0, 0, 0 ,0) NeedleCoordinates to the targeted_needle_coordinates.
+    populate_transformation_matrix(transformation_matrix, targeted_needle_coordinates);
 
-    // Array with table vertices plus an empty slot for table centroid:
-    gsl_vector* initial_table_coordinates[NUM_OF_TRANSFORMED_VECTORS];
+    // Array of all vectors describing our 3D model.
+    gsl_vector* initial_3dmodel[NUM_OF_TRANSFORMED_VECTORS];
     for (int i = 0; i < NUM_OF_TRANSFORMED_VECTORS; i++) {
-        initial_table_coordinates[i] = gsl_vector_alloc(3);
+        initial_3dmodel[i] = gsl_vector_alloc(3);
     }
-    populate_hexagon(initial_table_coordinates, TABLE_RADIUS, TABLE_ANGLE);
-    populate_centroid_coordinates(initial_table_coordinates);
-    populate_needle_coordinates(initial_table_coordinates);
-    // Transformation of all vectors (including centroid):
-    gsl_vector* transformed_table_coordinates[NUM_OF_TRANSFORMED_VECTORS];
+    populate_table_3dmodel(initial_3dmodel);
+
+    // Transformation of all vectors:
+    gsl_vector* transformed_3dmodel[NUM_OF_TRANSFORMED_VECTORS];
     for (int i = 0; i < NUM_OF_TRANSFORMED_VECTORS; i++) {
-        transformed_table_coordinates[i] = gsl_vector_alloc(3);
-        populate_transformed_coordinates(transformed_table_coordinates[i], transformation_matrix, initial_table_coordinates[i]);
+        transformed_3dmodel[i] = gsl_vector_alloc(3);
+        populate_transformed_coordinates(transformed_3dmodel[i], transformation_matrix, initial_3dmodel[i]);
     }
 
-    print_targeted_table_coordinates(targeted_table_coordinates);
+    print_targeted_needle_coordinates(targeted_needle_coordinates);
 
-    printf("\nTargeted (x, y, z) coordinates of table vertices and centroid and needle in cm:\n");
-    print_table_vertices_and_centroid_and_needle(transformed_table_coordinates, NUM_OF_TRANSFORMED_VECTORS);
+    printf("\nInitial (x, y, z) coordinates of the table 3d model points in cm:\n");
+    print_3dmodel_coordinates(initial_3dmodel, NUM_OF_TRANSFORMED_VECTORS);
 
-    printf("\nInitial (x, y, z) coordinates of table vertices and centroid and needle in cm:\n");
-    print_table_vertices_and_centroid_and_needle(initial_table_coordinates, NUM_OF_TRANSFORMED_VECTORS);
+    printf("\nTargeted (x, y, z) coordinates of the table 3d model points in cm:\n");
+    print_3dmodel_coordinates(transformed_3dmodel, NUM_OF_TRANSFORMED_VECTORS);
 
     gsl_vector* lowest_possible_slider_coordinates[NUM_OF_HEXAGON_VERTICES];
     for (int i = 0; i < NUM_OF_HEXAGON_VERTICES; i++) {
@@ -319,10 +325,10 @@ int main(void) {
     }
     populate_hexagon(lowest_possible_slider_coordinates, BASE_RADIUS, BASE_ANGLE);
 
-    printf("\nLowest possible slider slider coordinates (x, y, z) in cm:\n");
+    printf("\nLowest possible slider coordinates (x, y, z) in cm:\n");
     print_lowest_possible_slider_coordinates(lowest_possible_slider_coordinates, NUM_OF_HEXAGON_VERTICES);
 
-    OptionSliderCoordinates maybe_sliders = find_slider_coordinates(transformed_table_coordinates, lowest_possible_slider_coordinates);
+    OptionSliderCoordinates maybe_sliders = find_slider_coordinates(transformed_3dmodel, lowest_possible_slider_coordinates);
     print_transformed_slider_coordinates(maybe_sliders);
 
     return 0;
